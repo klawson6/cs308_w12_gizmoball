4c010cb62f719455ee090dbcca75e452fff91174
John McMenemy <john.mcmenemy.2017@uni.strath.ac.uk>
2019-02-03 10:26:29 +0000

    Added resizable canvas so that it re-draws and resizes itself according to the window

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,13 +1,21 @@
 package controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.BorderPane;
+import view.ResizableCanvas;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+
+    @FXML private ResizableCanvas canvas;
+    @FXML private BorderPane rootBorderPane;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        canvas.widthProperty().bind(rootBorderPane.widthProperty());
+        canvas.heightProperty().bind(rootBorderPane.heightProperty());
     }
 }
