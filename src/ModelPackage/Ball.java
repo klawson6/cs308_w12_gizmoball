1a8380b3f1ed412138fd8dfd5461f17581773d83
Kyle Lawson CESM2016 <kyle.lawson.2016@uni.strath.ac.uk>
2019-02-04 23:36:14 +0000

    Completed Ball class to represent the moving ball in the game. Sorted the Physics class to how it works for Cameron and I, we'll need to discuss why its acting up, sorry in advance if the Physics package rename messes things up for anyone! Ball class has a composing circle field that holds its position and radius, and a Vect to hold its current velocity. Getters are available for use when necessary. Methods have been included and commented with method specifications to modify the velocity, and to modify the position of the ball by replacing its composing circle with a new one, with the position of it computed using the current position values and computed movement in the x and y directions using the velocity and a given move time.

--- src/ModelPackage/Ball.java
+++ src/ModelPackage/Ball.java
@@ -1,20 +1,71 @@
 package ModelPackage;
 
+import Physics.Angle;
 import Physics.Circle;
 import Physics.Vect;
 
 public class Ball {
 
+    // Composing circle that contains the position of the ball.
     private Circle circle;
-    private Vect v;
-    private double xPosition;
-    private double yPosition;
+    // The velocity of the ball, has speed and direction.
+    private Vect velocity;
 
-    public Ball(double xPosition, double yPosition,double radius){
-
-        circle = new Circle(xPosition,yPosition,radius);
-
+    public Ball(double xPosition, double yPosition, double xVelocity, double yVelocity) {
+        // Build the composing circle of the playing ball with initial position and required diameter of 0.5L.
+        circle = new Circle(xPosition, yPosition, 0.25);
+        // Initialise the velocity field.
+        // Pass in the x and y components of the velocity.
+        // With this information it will calculate the resultant speed(length in the Vect class) and direction once relevant getters are called.
+        velocity = new Vect(xVelocity, yVelocity);
     }
 
+    // Return the speed component of the velocity of this ball.
+    public double getSpeed() {
+        return velocity.length();
+    }
 
+    // Return the angle component of the velocity of this ball.
+    public Angle getAngle() {
+        return velocity.angle();
+    }
+
+    // Return the current position of this ball as a Vect.
+    public Vect getPos() {
+        return circle.getCenter();
+    }
+
+    /**
+     * @requires velocity is not null
+     * @modifies this
+     * @effects Sets the current Vect velocity of this ball to the new Vect velocity given.
+     *          Returns true if this changed as a result, and returns false if it did not
+     * @param velocity
+     */
+    public boolean modifyVelocity(Vect velocity) {
+        if (velocity != null) {
+            this.velocity = velocity;
+            return true;
+        } else
+            return false;
+    }
+
+    /**
+     * @requires 0 < moveTime < 0.05
+     * @modifies this
+     * @effects Moves this ball by replacing its composing circle object with a new one.
+     *          The position of this new composing circle is computed using the velocity of this ball and the time for which is should move linearly.
+     *          Returns the Vect position of the new composing circle if this changed as a result, and returns false if it did not.
+     * @param moveTime
+     */
+    public Vect moveBall (double moveTime){
+        if (0 < moveTime && moveTime < 0.05){
+            Vect distanceVector = new Vect(velocity.angle(), velocity.length()*moveTime);
+            //circle = new Circle(circle.getCenter().x()+distanceVector.x(),circle.getCenter().y()+distanceVector.y(), 0.25);
+            circle = new Circle(circle.getCenter().plus(distanceVector), 0.25);
+            return circle.getCenter();
+        } else {
+            return null;
+        }
+    }
 }
