703bb4b554d5482cce777364cda517841b02c6ac
Cameron Taylor CES2016 <cameron.taylor.2016@uni.strath.ac.uk>
2019-02-08 18:04:17 +0000

    Edited Model and Gizmo Classes added methods for Key Connections and Gizmo Connections Added constructors to allow for Id to be passed in or created if no id is used. Added Getter for Ball. Added Getter for GizmoList.

--- src/ModelPackage/GSquare.java
+++ src/ModelPackage/GSquare.java
@@ -2,8 +2,11 @@ package ModelPackage;
 
 import Physics.Circle;
 import Physics.LineSegment;
+import javafx.scene.input.KeyEvent;
 
 import java.awt.*;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Set;
 
 public class GSquare implements Gizmo {
@@ -11,59 +14,88 @@ public class GSquare implements Gizmo {
     final private double coefficent = 1;
     private int xPos;
     private int yPos;
+    private HashSet<KeyEvent> keyBindings = new HashSet<>();
+    private String id;
+    private HashSet<Gizmo> connections = new HashSet<>();
 
     public GSquare(int xPos, int yPos){
         this.xPos = xPos;
         this.yPos = yPos;
+        id = "S" + xPos + yPos;
     }
 
-    @Override
+    public GSquare(int xPos, int yPos, String id){
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.id = id;
+    }
+
     public String getGizmoType() {
         return null;
     }
 
-    @Override
     public int getStartxPosition() {
         return xPos;
     }
 
-    @Override
     public int getStartyPosition() {
         return yPos;
     }
 
-    @Override
     public int getEndxPosition() {
         return xPos;
     }
 
-    @Override
     public int getEndyPosition() {
         return yPos;
     }
 
-    @Override
     public Color getColour() {
         return null;
     }
 
-    @Override
     public double getRotation() {
         return 0;
     }
 
-    @Override
     public Set<LineSegment> getComposingLines() {
         return null;
     }
 
-    @Override
     public double getReflectionCoef() {
         return coefficent;
     }
 
-    @Override
     public Set<Circle> getComposingCircles() {
         return null;
+    }
+
+    public void addKeyBinding(KeyEvent key) {keyBindings.add(key);}
+
+    public HashSet<KeyEvent> getKeybindings() {return keyBindings;}
+
+    public String getId() {
+        return id;
+    }
+
+    public void addGizmoConnection(Gizmo gizmo) {
+        connections.add(gizmo);
+    }
+
+    public void removeGizmoConnection(Gizmo gizmo) {
+        connections.remove(gizmo);
+    }
+
+    public Set<String> getGizmoConnectionIds() {
+        Set<String> ids = new HashSet<>();
+        for(Gizmo gizmos: connections){
+            ids.add(gizmos.getId());
+        }
+        return ids;
+    }
+
+    @Override
+    public void Rotate(double degrees) {
+        //Does nothing for square
     }
 }
--- src/ModelPackage/Gizmo.java
+++ src/ModelPackage/Gizmo.java
@@ -2,30 +2,45 @@ package ModelPackage;
 
 import Physics.Circle;
 import Physics.LineSegment;
+import javafx.scene.input.KeyEvent;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Set;
 
-public interface Gizmo{
+public interface Gizmo extends IGizmo{
 
     String getGizmoType();
 
     int getStartxPosition();
 
-    int getStartyPosition();
+    public int getEndxPosition();
 
-    int getEndxPosition();
+    public int getEndyPosition();
 
-    int getEndyPosition();
+    public double getRotation();
 
-    Color getColour();
-
-    double getRotation();
+    public Color getColour();
 
     Set<LineSegment> getComposingLines();
 
     double getReflectionCoef();
 
     Set<Circle> getComposingCircles();
+
+    void addKeyBinding(KeyEvent key);
+
+    HashSet<KeyEvent> getKeybindings();
+
+    String getId();
+
+    void addGizmoConnection(Gizmo gizmo);
+
+    void removeGizmoConnection(Gizmo gizmo);
+
+    Set<String> getGizmoConnectionIds();
+
+    void Rotate(double degrees);
 
 }
