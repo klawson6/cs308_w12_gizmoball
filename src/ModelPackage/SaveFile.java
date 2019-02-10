5d1a37f03acebfa336bf4c96a0c2588210a42245
Mark Falconer <mark.falconer.2016@uni.strath.ac.uk>
2019-02-10 23:21:05 +0000

    Began to implement save file, its done kinda messily, like with a save file object being created each time so this can be made better in the future.

--- src/ModelPackage/SaveFile.java
+++ src/ModelPackage/SaveFile.java
@@ -11,43 +11,36 @@ public class SaveFile {
 
     public SaveFile(String filename){
         this.filename = filename;
+        System.out.println("Saving to file " + filename);
     }
 
     public void save(Model model){
-        HashSet<Gizmo> gizmos = new HashSet<>();
-//        HashSet<Gizmo> gizmos = model.getGizmos();
-        for(Gizmo gizmo: gizmos){
-            String type  = "";
-//            String type = gizmo.getType();
+        String toSave;
 
-            switch(type){
-                case "Square":
-                    break;
-                case "Circle":
-                    break;
-                case "Triangle":
-                    break;
-                case "Left Flipper":
-                    break;
-                case "Right Flipper":
-                    break;
-                case "KeyConnect":
-                    break;
-                case "Connect":
-                    break;
-                case "Ball":
-                    break;
-                case "Absorber":
-                    break;
-                default:
-                    System.out.println("Shouldn't be possible");
-                    System.out.println("Type is " + type);
-                    break;
-            }
+        ///////////////////////////////////////////// General Gizmos /////////////////////////////////////////////
+        HashSet<Gizmo> gizmos = model.getGizmoList();
+        for(Gizmo gizmo: gizmos){
+            String type = gizmo.getGizmoType();
+
+            toSave = type + " " + gizmo.getId() + " " + gizmo.getStartxPosition() + " " + gizmo.getStartyPosition();
+
+
+            //todo fix absorbers as they dont seem to have a name in Murrays example file.
+
+            System.out.println(toSave);
+            //actually save
         }
 
+        ///////////////////////////////////////////// Ball /////////////////////////////////////////////
+        Ball ball = model.getBall();
+        toSave = "Ball B " + ball.getPos().x() + " " + ball.getPos().y() + " " + ball.getVelocity().x() + " " + ball.getVelocity().y();
+        System.out.println(toSave);
 
+        ///////////////////////////////////////////// Key Connects /////////////////////////////////////////////
+        //todo add key connects
 
+        ///////////////////////////////////////////// Connections /////////////////////////////////////////////
+        //todo add connections
 
     }
 
