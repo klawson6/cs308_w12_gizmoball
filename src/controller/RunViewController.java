3ecb6c72ffa8622baba280e8be6864c650805d95
John McMenemy <john.mcmenemy.2017@uni.strath.ac.uk>
2019-02-20 10:16:05 +0000

    Fixed resizable Canvasgit status

--- src/controller/RunViewController.java
+++ src/controller/RunViewController.java
@@ -11,6 +11,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ToolBar;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
@@ -36,22 +37,28 @@ public class RunViewController implements Initializable, Observer {
     @FXML private ResizableCanvas canvas;
     @FXML private VBox rootPane;
   //  @FXML private Label speed;
-    @FXML private Button quitButton;
-    @FXML private Button buildButton;
-    @FXML private Button saveButton;
-    @FXML private Button loadButton;
-    @FXML private Button startButton, stopButton, tickButton, buildButton, saveButton, loadButton, quitButton ;
+    @FXML private Button startButton, stopButton, tickButton, buildButton, saveButton, loadButton, quitButton;
+    @FXML private ToolBar toolBar;
 
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        initialiseCanvas();
+
         addButtonListeners();
 
         canvas.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent ->
                 System.out.println(mouseEvent.getX() + " " + mouseEvent.getY()));
 
+    }
+
+    private void initialiseCanvas(){
+        canvas.widthProperty().bind(rootPane.widthProperty());
+        canvas.heightProperty().bind(rootPane.heightProperty().subtract(toolBar.heightProperty()));
+        canvas.widthProperty().addListener(observable -> canvas.draw());
+        canvas.heightProperty().addListener(observable -> canvas.draw());
     }
 
     private void addButtonListeners(){
@@ -108,8 +115,22 @@ public class RunViewController implements Initializable, Observer {
         canvas.draw();
     }
 
-            this.model = model;
+    private void loadFile(){
+        File file = new FileChooser().showOpenDialog(stage);
+        if(file != null){
+            LoadFile r = new LoadFile(file);
+            model = r.run();
+            model.addObserver(this);
+            update((Observable) model, null);
+        }else{
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText("Please pick a file.");
+            alert.showAndWait();
+        }
 
+    }
 
     private void startTimeline(){
         setHandlers();
