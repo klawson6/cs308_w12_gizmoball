695b3c159f74dc4a29ebbe00f2ac067eb71900ef
John McMenemy <john.mcmenemy.2017@uni.strath.ac.uk>
2019-03-13 14:34:35 +0000

    Added IGizmo tests on Circle plus JUnit library to repo

---
+++ Test/ModelPackage/IGizmoCircleTest.java
@@ -0,0 +1,74 @@
+package ModelPackage;
+
+import javafx.scene.paint.Color;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IGizmoCircleTest {
+
+    IGizmo gizmo;
+
+    @BeforeEach
+    void setUp() {
+        gizmo = new GCircle(0, 0, "TestCircle");
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getGizmoType(){
+        assertEquals(gizmo.getGizmoType(), GizmoType.CIRCLE);
+    }
+
+    @Test
+    void getStartxPosition(){
+        assertEquals(gizmo.getStartxPosition(), 0);
+    }
+
+    @Test
+    void getStartyPosition(){
+        assertEquals(gizmo.getStartyPosition(), 0);
+    }
+
+    @Test
+    void getEndxPosition(){
+        assertEquals(gizmo.getEndxPosition(), 0);
+    }
+
+    @Test
+    void getEndyPosition(){
+        assertEquals(gizmo.getEndyPosition(), 0);
+    }
+
+    @Test
+    void getRotation(){
+        assertEquals(gizmo.getRotation(), 0);
+    }
+
+    @Test
+    void getColour(){
+        assertEquals(gizmo.getColour().getClass(), Color.class);
+    }
+
+    @Test
+    void activate(){
+
+    }
+
+    @Test
+    void deactivate(){
+
+    }
+
+    @Test
+    void getKeybindings(){
+
+    }
+
+
+}
